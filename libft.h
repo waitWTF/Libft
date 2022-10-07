ad9ab87ae7fd60f2f58b03a0760a7d6743aaf4a6
Thomas Andel <thandel@c3r3p5.42vienna.com>
2022-10-07 15:59:47 +0200

    files

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thandel <thandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 12:43:38 by thandel           #+#    #+#             */
-/*   Updated: 2022/10/07 14:35:25 by thandel          ###   ########.fr       */
+/*   Updated: 2022/10/07 15:08:56 by thandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		ft_isalnum(int c);
 int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-int		ft_strlen(int c);
+char	ft_strlen(char *src);
 int		ft_memset(int c);
 int		ft_bzero(int c);
 int		ft_memcpy(int c);
