3d84140a30c09415c1bc4c9b1c8fc3e2e1e98019
Thomas Andel <thandel@c3r3p5.42vienna.com>
2022-10-07 14:54:33 +0200

    files

--- libft.h
+++ libft.h
@@ -6,15 +6,38 @@
 /*   By: thandel <thandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 12:43:38 by thandel           #+#    #+#             */
-/*   Updated: 2022/10/03 12:53:23 by thandel          ###   ########.fr       */
+/*   Updated: 2022/10/07 14:35:25 by thandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef libft.a
-# define libft.a
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <errno.h>
+# include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(int c);
+int		ft_memset(int c);
+int		ft_bzero(int c);
+int		ft_memcpy(int c);
+int		ft_memmove(int c);
+char	*ft_strlcpy(char src, char dest, unsigned int c);
+char	*ft_strlcat(char src, char dest, unsigned int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(char c);
+char	ft_strrchr(char c);
+char	ft_strncmp(char src, char dest, unsigned int n);
+int		ft_memchr(int c);
+int		ft_memcmp(int c);
+char	*ft_strnstr(char src);
+int		ft_atoi(int c);
 
 #endif
